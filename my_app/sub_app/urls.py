41dc80d26dccdd470bd716c16ec822eb328f0a5e
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-06-26 01:50:05 -0700

    updatedd

--- my_app/sub_app/urls.py
+++ my_app/sub_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app import views
+from sub_app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
