417f000db9d893e01cee2fd788b6faa7a8f9ef4e
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-06-26 01:21:11 -0700

    updated

--- my_app/sub_app/admin.py
+++ my_app/sub_app/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from . models import MyModel
+from .models import Customer, Product, Cart, OrderPlaced
 
-# Register your models here.
-admin.site.register(MyModel)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'name', 'locality', 'city', 'zipcode', 'state']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'selling_price', 'discounted_price', 'brand', 'category']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'quantity']
+
+@admin.register(OrderPlaced)
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']
+
+from .models import BlogPost
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'updated_at', 'author')
+    ordering = ('-created_at',)
+
+admin.site.register(BlogPost, BlogPostAdmin)
